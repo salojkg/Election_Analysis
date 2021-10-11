222ba2839997c2242dad3e3ee135f71e9c91d715
salojkg <90934630+salojkg@users.noreply.github.com>
2021-10-11 18:45:20 -0500

    3.5.1-total votes

--- Pypoll.py
+++ Pypoll.py
@@ -10,13 +10,21 @@ import csv
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+# 1. Initialize a total vote counter
+total_votes=0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
+      # Read the header row.
+    headers = next(file_reader)
+
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+     # 2. Add to toal vote count
+        total_votes += 1
+
+        print(total_votes)
      
 
