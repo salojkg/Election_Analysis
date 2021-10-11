118430975e425084e43cca4327c45e247fb64d7f
salojkg <90934630+salojkg@users.noreply.github.com>
2021-10-11 18:56:25 -0500

    3.5.4- total votes for each candidate

--- Pypoll.py
+++ Pypoll.py
@@ -12,6 +12,11 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 # 1. Initialize a total vote counter
 total_votes=0
+# Candidate Options
+candidate_options = []
+
+# 1. Declare the empty dictionary.
+candidate_votes = {}
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -24,7 +29,19 @@ with open(file_to_load) as election_data:
     for row in file_reader:
      # 2. Add to toal vote count
         total_votes += 1
+# Print the candidate name from each row.
+        candidate_name = row[2]
 
-        print(total_votes)
+         # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+    # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+# Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+
+# Print the candidate vote dictionary.
+print(candidate_votes)
      
 
